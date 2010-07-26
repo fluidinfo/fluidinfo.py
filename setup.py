1326a84f679752505c10df9697277270e62b3ebd
Nicholas Tollervey <ntoll@ntoll.org>
2010-07-26 11:55:40 +0100

    Fixed dependency requirement for httplib2 in setup.py - thanks Martin v.Löwis for the bug report

--- setup.py
+++ setup.py
@@ -3,13 +3,14 @@
 from distutils.core import setup
 
 setup(name='fluiddb.py',
-      version='1.0.4',
+      version='1.0.5',
       description="A thin wrapper for FluidDB's RESTful API",
       author='Nicholas Tollervey (based upon work by Sanghyeon Seo)',
       author_email='ntoll@ntoll.org',
       url='http://fluidinfo.com',
       py_modules=['fluiddb',],
       license='MIT',
+      requires=['httplib2',],
       long_description=open('README').read(),
       classifiers=['Development Status :: 4 - Beta',
                    'Environment :: Web Environment',
